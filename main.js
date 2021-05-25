be1dfcd8622e72782f61863218d10882bdbc0fc7
payalsrao <75577552+payalsrao@users.noreply.github.com>
2021-05-25 23:37:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+function preload() {
+
+}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+}
+
+function draw() {
+
+}
+
+function take_snapshot(){    
+  save('LipstickFilterImage.png');
+}
+function selfie(){
+  window.alert ("SMILE PLEASE AND GET READY FOR THE CLOWN NOSE FILTER!!!!!!");
+}
